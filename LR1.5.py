711740b2944eb1d979e1a1b65a1623854ef7ecaa
Korepanowa <91186968+Korepanowa@users.noreply.github.com>
2023-10-25 15:31:50 +0500

    Update LR1.5.py

--- LR1.5.py
+++ LR1.5.py
@@ -1,30 +1,23 @@
 import wikipedia
 import json
 
-
-# распечатать резюме того, что такое питон
-#print(wikipedia.summary("Python Programming Language"))
-
 result = wikipedia.search("the rarest species of frogs")
-#print(result)
 
-# получить страницу: Neural network
+# Получим страницу.
 page = wikipedia.page(result[0])
 
-# получим заголовок страницы
+# Получим заголовок страницы.
 title = page.title
 
-# получение категорий страницы
+# Получение категорий страницы.
 categories = page.categories
 
-# получить весь текст (содержание) страницы Википедии
+# Получим весь текст (содержание) страницы Википедии.
 content = page.content
 
-# получение всех ссылок на странице
+# Получение всех ссылок на странице.
 links = page.links
 
-
-# в итоге
 summary = page.summary
 
 data = {
@@ -36,5 +29,4 @@ data = {
 
 
 j = json.dumps(data)
-# напечатаем инфо
 print(j)
